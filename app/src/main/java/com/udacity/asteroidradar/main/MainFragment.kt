123da7adfbd1f7926eb55d50d609a74a4099c9aa
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-11 10:28:28 +0300

    MainFragment.kt - Setup menu

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -2,43 +2,48 @@ package com.udacity.asteroidradar.main
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), MenuProvider {
 
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+    private val viewModel: MainViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding = FragmentMainBinding.inflate(inflater)
-        binding.lifecycleOwner = this
-
-        binding.viewModel = viewModel
-
-        // TODO switch to new approach
-        @Suppress("DEPRECATION")
-        setHasOptionsMenu(true)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
+        val binding = setDataBinding(inflater)
         return binding.root
     }
 
-    // TODO switch to new approach
-    @Suppress("DEPRECATION")
-    @Deprecated("Deprecated in Java")
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.main_overflow_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
+    private fun setDataBinding(inflater: LayoutInflater) = FragmentMainBinding.inflate(inflater).also {
+        it.lifecycleOwner = this
+        it.viewModel = viewModel
     }
 
-    @Deprecated("Deprecated in Java", ReplaceWith("true"))
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupMenu()
+    }
+
+    private fun setupMenu() {
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    // MenuProvider
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.main_overflow_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.show_all_menu -> {}
+            R.id.show_buy_menu -> {}
+            R.id.show_rent_menu -> {}
+            else -> return false
+        }
         return true
     }
 }
