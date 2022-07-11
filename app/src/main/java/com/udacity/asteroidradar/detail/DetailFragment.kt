6967ba09e2c28439070204c9b2e3e04e191245e4
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-11 10:47:43 +0300

    DetailFragment.kt - refactoring

--- app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/detail/DetailFragment.kt
@@ -7,28 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        val binding = FragmentDetailBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+    private val args: DetailFragmentArgs by navArgs()
 
-        val asteroid = DetailFragmentArgs.fromBundle(arguments!!).selectedAsteroid
-
-        binding.asteroid = asteroid
-
-        binding.helpButton.setOnClickListener {
-            displayAstronomicalUnitExplanationDialog()
-        }
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
+        val binding = setDataBinding(inflater)
         return binding.root
     }
 
+    private fun setDataBinding(inflater: LayoutInflater) = FragmentDetailBinding.inflate(inflater).also {
+        it.lifecycleOwner = this
+        it.asteroid = args.selectedAsteroid
+        it.helpButton.setOnClickListener {
+            displayAstronomicalUnitExplanationDialog()
+        }
+    }
+
     private fun displayAstronomicalUnitExplanationDialog() {
-        val builder = AlertDialog.Builder(activity!!)
+        val builder = AlertDialog.Builder(requireActivity())
             .setMessage(getString(R.string.astronomica_unit_explanation))
             .setPositiveButton(android.R.string.ok, null)
         builder.create().show()
