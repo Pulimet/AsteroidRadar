299860bf865490d89f645a7afc669c0ef6dae2c5
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-12 19:08:38 +0300

    Make sure any external API key that you utilize, has been removed from your code.

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -2,6 +2,6 @@ package com.udacity.asteroidradar
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
-    const val API_KEY = "hgMCVoNdxjYGt1he02ttzhOWbekr4urdXauC7b5V"
+    const val API_KEY = BuildConfig.NASA_API_KEY
     const val BASE_URL = "https://api.nasa.gov/"
 }
--- app/src/main/java/com/udacity/asteroidradar/db/NasaDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/db/NasaDao.kt
@@ -8,11 +8,6 @@ import java.util.*
 @Dao
 interface NasaDao {
 
-    // TODO If you're submitting via a public Github repository, please make sure any external API key that you utilize, has been removed from your code.
-    //  It's highly unsafe (and often breaks the Terms of Service) to include API keys in public repos, so you need to remove yours.
-    //  You can add a note in a README file where a reviewer should go to insert their API key. Reviewers have been trained to expect this situation.
-    // TODO Check the Project Rubric to confirm that you have met all of the requirements: https://review.udacity.com/#!/rubrics/2851/view
-
     @Query("SELECT * FROM asteroids WHERE date >= :fromDate ORDER BY date")
     fun getAsteroids(fromDate: Date = Date()): Flow<List<Asteroid>>
 
