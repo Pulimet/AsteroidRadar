1625d0022833d223056daf02a6cee3072b23e5e2
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-12 12:41:08 +0300

    Add content description to the views: Details images and dialog button. Check if it works correctly with talk back. Part 2

--- app/src/main/java/com/udacity/asteroidradar/ui/BindingAdapters.kt
+++ app/src/main/java/com/udacity/asteroidradar/ui/BindingAdapters.kt
@@ -54,8 +54,10 @@ fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
 fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
     if (isHazardous) {
         imageView.setImageResource(R.drawable.asteroid_hazardous)
+        imageView.contentDescription = imageView.context.getString(R.string.status_potentially_hazardous)
     } else {
         imageView.setImageResource(R.drawable.asteroid_safe)
+        imageView.contentDescription = imageView.context.getString(R.string.status_normal)
     }
 }
 
