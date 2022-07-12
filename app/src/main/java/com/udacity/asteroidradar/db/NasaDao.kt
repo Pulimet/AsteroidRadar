11974fbeb397f5690e4d2ecf8b16fb005897e0aa
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-12 09:32:00 +0300

    Add TODOs

--- app/src/main/java/com/udacity/asteroidradar/db/NasaDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/db/NasaDao.kt
@@ -6,6 +6,15 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NasaDao {
+    // TODO Fetch only Asteroids from today
+    // TODO Sort Asteroid by date
+    // TODO Be able to cache the data of the asteroid by using a worker, so it downloads and saves today's asteroids in
+    //  background once a day when the device is charging and wifi is enabled.
+    // TODO Add content description to the views: Details images and dialog button. Check if it works correctly with talk back.
+    // TODO Make sure the entire app works without an internet connection. (Picture of the day for example)
+    // TODO Modify the app to support multiple languages, device sizes, and orientations.
+    // TODO Make the app delete asteroids from the previous day once a day using the same workManager that downloads the asteroids.
+    // TODO Match the styles for the details screen subtitles and values to make it consistent, and make it look like what is in the designs.
     @Query("SELECT * from asteroids")
     fun getAsteroids(): Flow<List<Asteroid>>
 
