3a0bd6f5d39d2fd61ac91c02fe420242588a544e
Korolev, Alexey (ak3140) <ak3140@intl.att.com>
2022-07-12 09:33:23 +0300

    Add TODOs 2

--- app/src/main/java/com/udacity/asteroidradar/db/NasaDao.kt
+++ app/src/main/java/com/udacity/asteroidradar/db/NasaDao.kt
@@ -15,6 +15,10 @@ interface NasaDao {
     // TODO Modify the app to support multiple languages, device sizes, and orientations.
     // TODO Make the app delete asteroids from the previous day once a day using the same workManager that downloads the asteroids.
     // TODO Match the styles for the details screen subtitles and values to make it consistent, and make it look like what is in the designs.
+    // TODO If you're submitting via a public Github repository, please make sure any external API key that you utilize, has been removed from your code.
+    //  It's highly unsafe (and often breaks the Terms of Service) to include API keys in public repos, so you need to remove yours.
+    //  You can add a note in a README file where a reviewer should go to insert their API key. Reviewers have been trained to expect this situation.
+    // TODO Check the Project Rubric to confirm that you have met all of the requirements: https://review.udacity.com/#!/rubrics/2851/view
     @Query("SELECT * from asteroids")
     fun getAsteroids(): Flow<List<Asteroid>>
 
